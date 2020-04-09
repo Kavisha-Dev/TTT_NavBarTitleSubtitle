4cbdf09d724c37aed2cf8093d31a97c87b3fffd9
kavisha <kavisha_jg@yahoo.com>
2020-04-09 16:05:39 +0530

    version 1 - all files committed

---
+++ TTT_NavBarTitleSubtitle/UINavigationItem+Extensions.swift
@@ -0,0 +1,32 @@
+//
+//  UINavigationItem+Extensions.swift
+//  TTT_NavBarTitleSubtitle
+//
+//  Created by Kavisha on 09/04/20.
+//  Copyright © 2020 Kavisha. All rights reserved.
+//
+
+import UIKit
+
+extension UINavigationItem {
+    
+    func setTitle(title: String, subtitle: String) {
+        let titleLabel = UILabel()
+        titleLabel.text = title
+        titleLabel.font = .preferredFont(forTextStyle: UIFont.TextStyle.headline)
+        titleLabel.textColor = UIColor.black
+        
+        let subtitleLabel = UILabel()
+        subtitleLabel.text = subtitle
+        subtitleLabel.font = .preferredFont(forTextStyle: UIFont.TextStyle.subheadline)
+        subtitleLabel.textColor = UIColor.gray
+        
+        let stackView = UIStackView(arrangedSubviews: [titleLabel, subtitleLabel])
+        stackView.distribution = .equalCentering
+        stackView.alignment = .center
+        stackView.axis = .vertical
+        stackView.backgroundColor = UIColor.green
+        
+        self.titleView = stackView
+    }
+}
